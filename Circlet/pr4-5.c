b959c546b470c7774a79fbeeb14f6f45c8785401
rutu-moradiya <rutumoradiya500@gmail.com>
2024-08-28 17:22:26 +0530

    Add files via upload

---
+++ Circlet/pr4-5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,s;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }        
+        for(s=1;s<=5-i;s++)
+        {
+            printf("    ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        {
+            printf("\n");
+        }
+
+
+    }
+}
